afd58c5e9e384ec454a0105d32e4c13f21739d65
wutong <tong.wu@8win.com>
2018-05-29 13:58:10 +0800

    new 2 resolvers: setRole & like post.

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -11,6 +11,20 @@ const resolvers = {
     },
   },
   Mutation: {
+    async setRole(parent, { id, newRole }, ctx, info) {
+      const { id: userId, role } = await getUser(ctx)
+      if (role !== 'ADMIN') {
+        throw new Error('操作者没有足够的权限')
+      }
+      return ctx.db.mutation.updateUser({
+        where: {
+          id,
+        },
+        data: {
+          role: newRole,
+        }
+      })
+    },
     async createDraft(parent, { title, content, likes }, ctx, info) {
       const { id: userId } = await getUser(ctx)
       return ctx.db.mutation.createPost(
@@ -39,6 +53,23 @@ const resolvers = {
         info,
       )
     },
+    async like(parent, { id, amount = 1 }, ctx: Context, info) {
+      const post = await ctx.db.query.post({
+        where: {id}
+        }, `{
+        id
+        likes
+      }`)
+      if (!post.id) {
+        throw new Error(`没有找到对应的文章`)
+      }
+      return ctx.db.mutation.updatePost({
+        where: { id },
+        data: {
+          likes: post.likes + amount
+        }
+      })
+    }
   },
 }
 
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -7,5 +7,6 @@ export function getUser(ctx) {
     }
   }, `{
       id
+      role
     }`)
 }
