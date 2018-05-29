94ebc2a32add1878d8b1418e94227d4f56826bd7
wutong <tong.wu@8win.com>
2018-05-29 11:45:53 +0800

    remove prisma binding temporary for clearify. use in memory store data instead.

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,50 +1,42 @@
 import { Context } from './interfaces'
 import { getUser } from './utils/auth'
 
+
+let idCount = 0
+const posts = []
+
 const resolvers = {
   Query: {
-    feed(parent, args, ctx: Context, info) {
-      console.log()
-      return ctx.db.query.posts({
-        where: {
-          isPublished: true
-        },
-        orderBy: 'createdAt_DESC'
-      }, info)
-    },
-    drafts(parent, args, ctx: Context, info) {
-      return ctx.db.query.posts({
-        where: {
-          isPublished: false
-        },
-        orderBy: 'createdAt_DESC'
-      }, info)
-    },
-    post(parent, { id }, ctx: Context, info) {
-      return ctx.db.query.post({
-        where: {
-          id,
-        },
-      }, info)
-    },
+    description: () => `This is the API for a simple blogging application`,
+    posts: () => posts,
+    post: (parent, args) => posts.find(post => post.id === args.id),
   },
   Mutation: {
-    async createDraft(parent, {title, content}, ctx: Context, info) {
-      const { id: userId } = await getUser(ctx)
-      return ctx.db.mutation.createPost(
-        {
-          data: {
-            title,
-            content,
-            user: {
-              connect: { id: userId },
-            },
-            // isPublished: false,  #schema default false
-            // likes: 0,  #schema default 0
-          },
-        },
-        info
-      )
+    createDraft: (parent, args) => {
+      const post = {
+        id: `post_${idCount++}`,
+        title: args.title,
+        content: args.content,
+        likes: args.likes || 0,
+        published: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+      posts.push(post)
+      return post
+    },
+    deletePost: (parent, args) => {
+      const postIndex = posts.findIndex(post => post.id === args.id)
+      if (postIndex > -1) {
+        const deleted = posts.splice(postIndex, 1)
+        return deleted[0]
+      }
+      return null
+    },
+    publish: (parent, args) => {
+      const postIndex = posts.findIndex(post => post.id === args.id)
+      posts[postIndex].published = true
+      return posts[postIndex]
     },
   },
 }
